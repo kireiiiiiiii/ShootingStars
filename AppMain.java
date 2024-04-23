1864e576e34f8cff4bf08849826d83f7c4269904
maty <matysta@outlook.com>
2024-04-23 13:07:56 -0700

    Added main and constructor to AppMain, added class headers

--- AppMain.java
+++ AppMain.java
@@ -1,3 +1,19 @@
-public class AppMain {
-    
+/**
+ * Author: Matěj Šťastný
+ * Date created: 4/23/2024
+ * 
+ * TODO: Class header
+ */
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class AppMain extends JFrame {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new AppMain()); //TODO what does this do?
+    }
+
+    public AppMain() {
+        setVisible(true);
+    }
 }
--- CircleClickerPanel.java
+++ CircleClickerPanel.java
@@ -1,3 +1,12 @@
-class CircleClickerPanel {
+/**
+ * Author: Matěj Šťastný
+ * Date created: 4/23/2024
+ * 
+ * TODO: Class header
+ */
+
+import javax.swing.JPanel;
+
+class CircleClickerPanel extends JPanel{
     
 }
