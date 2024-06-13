45879c7d55ebba5945630269477f809b34bdd55f
Matěj Šťastný <matysta@outlook.com>
2024-06-13 13:29:40 -0700

    Added a screen util class

---
+++ src/main/java/com/example/Tools/ScreenUtil.java
@@ -0,0 +1,65 @@
+/*
+ * Author: Matěj Šťastný
+ * Date created: 
+ * Github link: https://github.com/kireiiiiiiii/TargetGame
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+package com.example.Tools;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+/**
+ * Class to get information from the user screen.
+ * 
+ */
+public class ScreenUtil {
+    
+    /**
+     * Calculates the desired size of the window depening on the dimensions of the
+     * user screen.
+     * 
+     * @param dimentions - user's screen dimensions.
+     * @return {@code double} position array.
+     */
+    public static double[] getAppWindowSize(int[] screenDimentions) {
+        double x = screenDimentions[0] * 0.9;
+        double y = screenDimentions[1] * 0.9;
+        double[] dimensions = { x, y };
+        return dimensions;
+    }
+
+    /**
+     * Gets the screen dimensions of the user's screen using {@code Toolkit}.
+     * 
+     * @return new {@code Position} object of the dimensions.
+     */
+    public static int[] getScreenDimensions() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = screenSize.width;
+        int screenHeight = screenSize.height;
+        int[] dimensions = { screenWidth, screenHeight };
+        return dimensions;
+    }
+
+}
