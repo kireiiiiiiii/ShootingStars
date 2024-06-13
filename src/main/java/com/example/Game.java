9476de40a95f7a76b8fe13869deb531fd6123236
Matěj Šťastný <matysta@outlook.com>
2024-06-13 13:21:57 -0700

    Added a Game object

---
+++ src/main/java/com/example/Game.java
@@ -0,0 +1,80 @@
+/*
+ * Author: Matěj Šťastný
+ * Date created: 6/13/2024
+ * Github link:  https://github.com/kireiiiiiiii/TargetGame
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+package com.example;
+
+import java.awt.event.*;
+
+/**
+ * Main game object.
+ * 
+ */
+public class Game {
+
+    /////////////////
+    // Variables
+    ////////////////
+
+    /////////////////
+    // Constructors
+    ////////////////
+    
+    public Game() {
+        //initiliaze the JPanel and JFrame
+        // Game loop
+        // Timer start
+        // Load the top score
+    }
+
+    /////////////////
+    // Events
+    ////////////////
+
+    public void onGameStarted() {
+
+    }
+
+    public void onGameRefreshed() {
+        
+    }
+
+    public void onGameEnd() {
+        // Switch the Panel 
+    }
+
+    public void onGameStart() {
+
+    }
+
+    public void onMouseClicked(MouseEvent e) {
+
+    }
+
+    public void onKeyClicked(KeyEvent e) {
+
+    }
+
+}
