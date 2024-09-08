484c9bc7129aa61cd6ad7c5d12bee2a416d7c1e3
Matěj Šťastný <matysta@outlook.com>
2024-09-08 08:43:27 -0700

    Added a method to get a spreadsheet cell value.

--- src/main/java/com/kireiiiiiiii/shooting_stars/AppMain.java
+++ src/main/java/com/kireiiiiiiii/shooting_stars/AppMain.java
@@ -30,6 +30,11 @@ import javax.swing.SwingUtilities;
 
 import com.kireiiiiiiii.shooting_stars.constants.Logs;
 
+import java.io.File;
+import java.io.InputStream;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 /**
  * Main method for
  * 
@@ -59,6 +64,42 @@ public class AppMain {
             game = new Game();
         };
         SwingUtilities.invokeLater(myApp);
+    }
+
+    // ! DEBUG MAIN METHOD
+    // public static void main(String[] args) {
+    // String cell = getCellValue("dialogue", "dialogue", 0, 0);
+    // System.out.println(cell);
+    // }
+
+    /**
+     * Returns the contents of an {@code .xlsx} spreadsheet cell as a {@code String}
+     * value.
+     * 
+     * @param fileName   - name of the spreadsheet file, {@code .xlsx} will be
+     *                   appended
+     * @param sheetName  - name of the target sheet name.
+     * @param rowNumber  - row number, first row is 0.
+     * @param cellNumber - the index of the target cell in the row, first cell is 0.
+     * @return a {@code String} value of the cell, {@code null} if empty.
+     */
+    public static String getCellValue(String fileName, String sheetName, int rowNumber, int cellNumber) {
+        String cellValue = null;
+        String path = File.separator + "spreadsheets" + File.separator + fileName + ".xlsx";
+        try (InputStream inputStream = AppMain.class.getResourceAsStream(path);
+                Workbook workbook = new XSSFWorkbook(inputStream)) {
+
+            Sheet sheet = workbook.getSheet(sheetName);
+            Row row = sheet.getRow(rowNumber);
+            Cell cell = row.getCell(cellNumber);
+
+            if (cell != null) {
+                cellValue = cell.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cellValue;
     }
 
 }
